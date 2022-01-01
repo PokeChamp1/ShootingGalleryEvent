ad9971e9b4427209af77f1c6485e3335d042a31d
PokeChamp1 <94212117+PokeChamp1@users.noreply.github.com>
2022-01-01 18:51:14 +0800

    Create FireballLauncher.java

---
+++ FireballLauncher.java
@@ -0,0 +1,48 @@
+package io.github.pokechamp1.shootinggalleryevent;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class FireballLauncher implements Listener, CommandExecutor {
+	 
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (sender instanceof Player) {
+
+
+        Player player = (Player) sender;
+        ItemStack fb = new ItemStack(Material.DIAMOND_HOE);
+        ItemMeta fbmeta = fb.getItemMeta();
+        fbmeta.setDisplayName(ChatColor.YELLOW + "Fireball Launcher");
+        fb.setItemMeta(fbmeta);
+        player.getInventory().addItem(fb); }
+        return true;
+  }
+@EventHandler 
+public void onRightClick ( PlayerInteractEvent e) { 
+	  System.out.println("-----PlayerInteract-----");
+	  System.out.println("Action Type: " + e.getAction().name());
+	  System.out.println("Holding: " + e.getPlayer().getItemInHand().getType().name());
+	  System.out.println("Cancelled: " + e.isCancelled());
+    Player p = e.getPlayer();
+     if (e.getAction() == Action.RIGHT_CLICK_BLOCK) { 
+            ItemStack item = p.getInventory().getItemInHand();
+            if (item.getType() == Material.DIAMOND_HOE) {
+                if(item.hasItemMeta() && 
+                        item.getItemMeta().hasDisplayName() && 
+                        item.getItemMeta().getDisplayName().equals(ChatColor.YELLOW + "Fireball Launcher")) {
+                    p.sendMessage(ChatColor.AQUA + "Ability Used!");
+              }
+            }
+          
+     } }  
+}
