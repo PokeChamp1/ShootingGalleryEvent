0e58fd8c147fde19b7bfc8472c99daf01415c266
PokeChamp1 <94212117+PokeChamp1@users.noreply.github.com>
2022-01-01 18:47:43 +0800

    Create ShootingGalleryEvent.java

---
+++ ShootingGalleryEvent.java
@@ -0,0 +1,17 @@
+package io.github.pokechamp1.shootinggalleryevent;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+public final class ShootingGalleryEvent extends JavaPlugin {
+	@Override
+    public void onEnable() { 
+		this.getCommand("fb").setExecutor(new FireballLauncher());
+
+		getServer().getPluginManager().registerEvents(new FireballLauncher(), this);
+    }
+	@Override
+    public void onDisable() {
+    }     
+ 
+ 
+}
