cbadef6cb372d7de94d69e783664e26f031a92ef
QB_poke <94212117+PokeChamp1@users.noreply.github.com>
2022-01-02 20:29:02 +0800

    Update ShootingGalleryEvent.java

--- ShootingGalleryEvent.java
+++ ShootingGalleryEvent.java
@@ -2,16 +2,18 @@ package io.github.pokechamp1.shootinggalleryevent;
 
 
 import org.bukkit.plugin.java.JavaPlugin;
-public final class ShootingGalleryEvent extends JavaPlugin {
-	@Override
-    public void onEnable() { 
-		this.getCommand("fb").setExecutor(new FireballLauncher());
+public final class ShootingGalleryEvent extends JavaPlugin 
+{
+    @Override
+    public void onEnable() 
+    { 
+        this.getCommand("fb").setExecutor(new FireballLauncher());
 
-		getServer().getPluginManager().registerEvents(new FireballLauncher(), this);
+        getServer().getPluginManager().registerEvents(new FireballLauncher(), this);
     }
-	@Override
-    public void onDisable() {
-    }     
- 
- 
+    @Override
+    public void onDisable() 
+    {
+
+    }      
 }
